b32cabacccd33d6858fb239f5a6cb8ee9f8e25c7
dukim <sajacall82@naver.com>
2023-09-11 17:20:34 +0900

    valid_parentheses update

---
+++ algorithm/valid_parentheses.py
@@ -0,0 +1,41 @@
+import time
+from typing import List
+
+
+def is_valid(s: str) -> bool:
+    pass
+
+def time_trace(func):
+    def wrapper(*args, **kwargs):
+        st = time.time()
+        rt = func(*args, **kwargs)
+        print(f'### time : {time.time() - st:.3f} ms')
+        return rt
+
+    return wrapper
+
+@time_trace
+def judge_test(solution, str, expected):
+    result = solution(str)
+    ret = (result == expected)
+    if ret:
+        print(f'success, func: {solution.__name__}, input: {str}')
+        return True
+    else:
+        print(
+            f'failed, func: {solution.__name__}, input: {str}, expect: {expected}, real: {result}')
+        return False
+
+def judge_helper(solution):
+    assets = [
+        # temperatures, wait
+        ("{}", True),
+        ("()[]{}", True),
+        ("(]", False),
+
+    ]
+    passed = sum([judge_test(solution, *asset) for asset in assets])
+    print(f'{len(assets)} tests, passed : {passed}, failed: {len(assets) - passed}\n')
+
+
+judge_helper(is_valid)
