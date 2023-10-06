ca0ab02f06c2df10ff213b1ec794c2cd4d449104
dukim <sajacall82@naver.com>
2023-10-06 12:24:55 +0900

    refactor: 나머지를 이용한 indexing
    불필요한 메모리 낭비를 없애기 위해 pattern에 나머지를 이용함

--- algorithm/search/42840_완전탐색.py
+++ algorithm/search/42840_완전탐색.py
@@ -3,24 +3,18 @@
 def solution(answers):
     answer = []
 
-    len_a = len(answers)
-
     pattern_1 = [1,2,3,4,5,]
     pattern_2 = [2,1,2,3,2,4,2,5,]
     pattern_3 = [3,3,1,1,2,2,4,4,5,5,]
 
-    pattern_1 = pattern_1 * int(len_a / len(pattern_1) + 1)
-    pattern_2 = pattern_2 * int(len_a / len(pattern_2) + 1)
-    pattern_3 = pattern_3 * int(len_a / len(pattern_3) + 1)
-
     same_count= [0, 0, 0]
     for idx, n in enumerate(answers):
-        if pattern_1[idx] == n:
-            same_count[0] +=1
-        if pattern_2[idx] == n:
-            same_count[1] +=1
-        if pattern_3[idx] == n:
-            same_count[2] +=1
+        if pattern_1[idx % len(pattern_1)] == n:
+            same_count[0] += 1
+        if pattern_2[idx % len(pattern_2)] == n:
+            same_count[1] += 1
+        if pattern_3[idx % len(pattern_3)] == n:
+            same_count[2] += 1
 
     max_ = max(same_count)
     for idx, s in enumerate(same_count):
